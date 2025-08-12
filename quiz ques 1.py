fc69c9e1cf859524aae194bfa252d78d66b81b4c
PrashantGupta8281 <guptaprashant2006@gmail.com>
2025-08-13 00:34:35 +0530

    Add files via upload

---
+++ quiz ques 1.py
@@ -0,0 +1,8 @@
+temp = int(input("note down the temperature in celsius"))
+if temp >= 25:
+    print("it is too hot")
+elif 15 < temp < 25:
+    print("it is warm")
+else: 
+    temp <= 15
+    print("it is too cold")
